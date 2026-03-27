c2cd30cddfbcc61d4e8059dd3b04327a6998cbbd
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-03-27 23:33:36 +0100

    Update mod.rs

--- src/components/auth/mod.rs
+++ src/components/auth/mod.rs
@@ -1,11 +1,11 @@
 pub mod login;
 pub mod signup;
 
-use leptos::prelude::*;
-use wasm_bindgen::JsCast;
-
 pub use login::*;
 pub use signup::*;
+
+use leptos::prelude::*;
+use crate::components::icons::IconShield;
 
 #[component]
 pub fn AuthPage() -> impl IntoView {
@@ -13,17 +13,58 @@ pub fn AuthPage() -> impl IntoView {
 
     view! {
         <div class="auth-page">
-            <div class="auth-container">
-                <div class="auth-header">
-                    <h1>"MidManStudio"</h1>
-                    <p>"Accounts"</p>
+            <div class="auth-panel">
+
+                <div class="auth-brand">
+                    <div class="auth-brand-logo">
+                        <IconShield class="icon-svg icon-lg" />
+                    </div>
+                    <div class="auth-brand-text">
+                        <span class="auth-brand-mms">"MmS"</span>
+                        <span class="auth-brand-accounts">"Accounts"</span>
+                    </div>
+                </div>
+
+                <div class="auth-tab-row">
+                    <button
+                        class=move || {
+                            if mode.get() == "signin" {
+                                "auth-tab auth-tab--active"
+                            } else {
+                                "auth-tab"
+                            }
+                        }
+                        on:click=move |_| set_mode.set("signin")
+                    >
+                        "Sign In"
+                    </button>
+                    <button
+                        class=move || {
+                            if mode.get() == "signup" {
+                                "auth-tab auth-tab--active"
+                            } else {
+                                "auth-tab"
+                            }
+                        }
+                        on:click=move |_| set_mode.set("signup")
+                    >
+                        "Create Account"
+                    </button>
                 </div>
 
                 {move || if mode.get() == "signin" {
-                    view! { <LoginForm on_switch=move || set_mode.set("signup") /> }.into_any()
+                    view! {
+                        <LoginForm on_switch=move || set_mode.set("signup") />
+                    }.into_any()
                 } else {
-                    view! { <SignupForm on_switch=move || set_mode.set("signin") /> }.into_any()
+                    view! {
+                        <SignupForm on_switch=move || set_mode.set("signin") />
+                    }.into_any()
                 }}
+
+                <div class="auth-footer-link">
+                    <a href="#" class="auth-back-home">"Back to home"</a>
+                </div>
             </div>
         </div>
     }
