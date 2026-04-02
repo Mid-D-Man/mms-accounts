00e3734db9b5716148665930603398dc69b813e0
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-04-02 05:59:37 +0100

    Update mod.rs

--- src/supabase/mod.rs
+++ src/supabase/mod.rs
@@ -2,4 +2,9 @@ pub mod client;
 pub mod auth;
 
 pub use client::*;
-pub use auth::{SignUpOutcome, generate_mid_secret, copy_to_clipboard};
+pub use auth::{
+    SignUpOutcome,
+    generate_mid_secret,
+    generate_path_id,
+    copy_to_clipboard,
+};
